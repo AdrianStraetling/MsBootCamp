c177144e9a61d471dab58cb148ebf8e1ab2919f5
root <root@minidev>
2017-12-05 22:10:32 +0100

    fixed thymeleaf

--- boot/src/main/java/com/example/demo/HelloController.java
+++ boot/src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,17 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-	@GetMapping(path = "/hello", produces = "application/json")
-	public String hello() {
-
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
 		return "hello";
 	}
 }
